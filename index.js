3166b37e0770b80e4747c8708887917444215ed2
matiascartes18 <matias.cartes@ug.uchile.cl>
2023-03-02 11:14:29 -0300

    second commit

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
+
 const app = express();
 
 app.use(cors());
-
+app.use(express.static('build'));
 app.use(express.json());
 
 app.use(
